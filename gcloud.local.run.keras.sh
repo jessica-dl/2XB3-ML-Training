7f3c1e53ec58f80dbf555ddfbf8d1bf978fe2dc3
SamCymbaluk <samuelcymbaluk@gmail.com>
2019-03-11 21:37:20 -0400

    Add initial working test version

---
+++ gcloud.local.run.keras.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gcloud ml-engine local train \
+  --module-name trainer.example5-keras \
+  --package-path ./trainer \
+  -- \
+  --train-file sentiment_set.pickle \
+  --job-dir ./tmp/example-5
