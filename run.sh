98050067e07d50c728d7d5133021eeee8acf3f88
SamCymbaluk <samuelcymbaluk@gmail.com>
2019-03-13 22:04:59 -0400

    Fix job_dir argument

--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@
 if [ "$#" -eq 1 ]
 then
     gcloud ml-engine jobs submit training "$1" \
-        --module-name=trainer.cnn_with_keras \
+        --module-name=trainer.trainer \
         --package-path=./trainer \
-        --bucket=gs://cs2xb3_ml/ \
-        --job-dir="$1" \
+        --job-dir=gs://cs2xb3_ml/"$1" \
         --dataset_url=gs://cs2xb3_ml/path/to/dataset \
         --region=us-east1 \
         --config=trainer/cloudml-gpu.yaml
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -3,24 +3,19 @@ from trainer.dataset import Dataset
 from trainer.models.aging_model import AgingModel
 
 
-def main(bucket, job_dir, dataset_url, **args):
+def main(job_dir, dataset_url, **args):
     data = []  # TODO get data from API
     dataset = Dataset(dataset_url, data)
 
     aging_model = AgingModel()
 
-    aging_model.train(dataset, bucket + job_dir + "/logs/tensorboard")
+    aging_model.train(dataset, job_dir + "/logs/tensorboard")
 
-    aging_model.save(bucket + job_dir + "/model.h5")
+    aging_model.save(job_dir + "/model.h5")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
-    # Input Arguments
-    parser.add_argument("--bucket",
-                        help="GCS cloud bucket",
-                        required=True)
 
     parser.add_argument("--job-dir",
                         help="GCS relative location within bucket to write checkpoints and export models",
